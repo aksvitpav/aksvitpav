bc0eca7196b390851695b25cd5fe952b6ad54d87
AksVitPav <aksvitpav@gmail.com>
2021-09-06 08:57:52 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- TailWind -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Title -->
+    <title>Сайт веб-разработчика AksVitPav</title>
+</head>
+<body>
+    <div class="container mx-auto">
+        AksVitPav
+    </div>
+</body>
+</html>
